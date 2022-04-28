9f09a0601dc02d163df9d74744fb11c226586023
C-4KE <glebkinkirill@gmail.com>
2022-04-28 17:42:22 +0300

    Добавлен класс Parser. Добавлен метод ParseJSON для чтения JSON файла с парами <имя_файла : хэш>.

---
+++ FileIntegrityController/FileIntegrityController/Parser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace FileIntegrityController
+{
+    class Parser
+    {
+        static Dictionary<string, string> ParseJSON(string jsonPath)
+        {
+            Dictionary<string, string> fileHash;
+            if (File.Exists(jsonPath))
+            {
+                string jsonString = File.ReadAllText(jsonPath);
+                if (jsonString.Length != 0)
+                {
+                    try
+                    {
+                        fileHash = JsonSerializer.Deserialize<Dictionary<string, string>>(jsonString);
+                    }
+                    catch (Exception exc)
+                    {
+                        Console.WriteLine("Failed to deserialize Json string: " + exc.Message);
+                        fileHash = null;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("There are no <file_name : hash> pairs in Json file.");
+                    fileHash = null;
+                }
+            }
+            else
+            {
+                Console.WriteLine("File \"" + jsonPath + "\" does not exist.");
+                fileHash = null;
+            }
+            return fileHash;
+        }
+    }
+}
