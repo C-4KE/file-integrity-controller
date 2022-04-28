efae66e029b3df678c38b9f9ebbd3628f8560f1a
C-4KE <glebkinkirill@gmail.com>
2022-04-28 21:53:24 +0300

    Добавлены тесты для Parser.ParseJSON(), проверяющие некорректные случаи.

--- FileIntegrityController/FileIntegrityControllerTests/ParserTests.cs
+++ FileIntegrityController/FileIntegrityControllerTests/ParserTests.cs
@@ -1,7 +1,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Text.Json;
+using System.Text;
 using System.Linq;
 
 using FileIntegrityController;
@@ -12,7 +14,7 @@ namespace FileIntegrityControllerTests
     public class ParserTests
     {
         [TestMethod]
-        public void ParseJSON_ExistingFileWithCorrectData_DictionaryWithCorrectPairs()
+        public void ParseJSON_ExistingFileWithCorrectData_ReturnIsDictionaryWithCorrectPairs()
         {
             // Arrange
             string jsonPath = "./TestJSON.json";
@@ -32,6 +34,53 @@ namespace FileIntegrityControllerTests
             // Assert
             Assert.IsTrue(expexted.Count == actual.Count && !expexted.Except(actual).Any());
             File.Delete(jsonPath);
+        }
+
+        [TestMethod]
+        public void ParseJSON_ExistingFileWithIncorrectData_ReturnIsNull()
+        {
+            // Arrange
+            string jsonPath = "./TestJSON.json";
+            using (FileStream fstream = new FileStream(jsonPath, FileMode.Create))
+            {
+                fstream.Seek(0, SeekOrigin.End);
+                fstream.Write(Encoding.UTF8.GetBytes("Incorrect data"));
+            }
+
+            // Act
+            Dictionary<string, string> actual = Parser.ParseJSON(jsonPath);
+
+            // Assert
+            Assert.IsNull(actual);
+            File.Delete(jsonPath);
+        }
+
+        [TestMethod]
+        public void ParseJSON_ExistingFileWithoutData_ReturnIsNull()
+        {
+            // Arrange
+            string jsonPath = "./TestJSON.json";
+            using (FileStream fstream = new FileStream(jsonPath, FileMode.Create)) { }
+
+            // Act
+            Dictionary<string, string> actual = Parser.ParseJSON(jsonPath);
+
+            // Assert
+            Assert.IsNull(actual);
+            File.Delete(jsonPath);
+        }
+
+        [TestMethod]
+        public void ParseJSON_NotexistingFile_ReturnIsNull()
+        {
+            // Arrange
+            string jsonPath = "./TestJSON.json";
+
+            // Act
+            Dictionary<string, string> actual = Parser.ParseJSON(jsonPath);
+
+            // Assert
+            Assert.IsNull(actual);
         }
     }
 }
