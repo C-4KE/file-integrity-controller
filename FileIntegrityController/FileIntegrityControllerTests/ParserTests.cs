6e6e927397c84942098a257b5aaae2f46c261914
C-4KE <glebkinkirill@gmail.com>
2022-04-28 20:14:36 +0300

    Создан проект для Unit-тестов.

---
+++ FileIntegrityController/FileIntegrityControllerTests/ParserTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileIntegrityControllerTests
+{
+    [TestClass]
+    public class ParserTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
