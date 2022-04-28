1f0352ffeea99855acd63799cf5a4371f30d25e3
C-4KE <glebkinkirill@gmail.com>
2022-04-28 23:16:22 +0300

    Добавлен метод для сортировки файлов с хешами по дискам.

--- FileIntegrityController/FileIntegrityController/Parser.cs
+++ FileIntegrityController/FileIntegrityController/Parser.cs
@@ -46,5 +46,80 @@ namespace FileIntegrityController
             }
             return fileHash;
         }
+
+        /**
+         * <summary>Метод, сортирующий словарь с парами (имя_файла : хеш) по дискам.</summary>
+         * <remarks>Если входной словарь не пустой, но содержит пары с несуществующими файлами / некорректными ключами, то онине добавляются в выходной массив.</remarks>
+         * <param name="filesHashes">Словарь с парами (имя_файла : хеш).</param>
+         * <returns>Возвращает массив объектов FileGroup, каждый из которых хранит информацию о файлах с одного диска. Если входной словарь пустой, возвращает null.</returns>
+         */
+        public static FileGroup[] SortFilesByDisks(Dictionary<string, string> filesHashes)
+        {
+            if (filesHashes.Count != 0)
+            {
+                FileGroup[] fileGroups = null;
+                List<string> disks = new List<string>();
+                foreach (KeyValuePair<string, string> fileHash in filesHashes)
+                {
+                    try
+                    {
+                        DriveInfo driveInfo = GetDriveInfo(fileHash.Key);
+                        if (driveInfo != null)
+                        {
+                            string drive = driveInfo.Name;
+                            if (disks.Contains(drive))
+                            {
+                                foreach (FileGroup fileGroup in fileGroups)
+                                {
+                                    if (fileGroup.DiskName == drive)
+                                    {
+                                        fileGroup.FilesHashes.Add(fileHash.Key, fileHash.Value);
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                disks.Add(drive);
+                                if (fileGroups == null)
+                                {
+                                    fileGroups = new FileGroup[1];
+                                }
+                                else
+                                {
+                                    Array.Resize<FileGroup>(ref fileGroups, fileGroups.Length + 1);
+                                }
+                                Dictionary<string, string> newDict = new Dictionary<string, string>();
+                                newDict.Add(fileHash.Key, fileHash.Value);
+                                fileGroups[fileGroups.Length - 1] = new FileGroup(drive, newDict);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Failed to add pair <" + fileHash.Key + "; " + fileHash.Value + ">: this file does not exist.");
+                        }
+                    }
+                    catch (Exception exc)
+                    {
+                        Console.WriteLine("Failed to add pair <" + fileHash.Key + "; " + fileHash.Value + ">: " + exc.Message);
+                    }
+                }
+                return fileGroups;
+            }
+            else
+            {
+                Console.WriteLine("Failed to sort files\' hashes by disks: dictionary is empty.");
+                return null;
+            }
+        }
+
+        /**
+         * <summary>Метод, возвращающий информацию о носителе, на котором находится файл.</summary>
+         * <param name="path">Путь к файлу</param>
+         * <returns>Объект DriveInfo, если файл существует. В обратном случае - null.</returns>
+         */
+        private static DriveInfo GetDriveInfo(string path)
+        {
+            return File.Exists(path) ? new DriveInfo((new FileInfo(path)).Directory.Root.FullName) : null;
+        }
     }
 }
