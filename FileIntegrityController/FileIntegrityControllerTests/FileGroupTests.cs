96692312f51c99e42463daa27038fde11ddfb529
C-4KE <glebkinkirill@gmail.com>
2022-05-01 13:57:24 +0300

    Добавлены тесты для метода FileGroup.Equals().

--- FileIntegrityController/FileIntegrityControllerTests/FileGroupTests.cs
+++ FileIntegrityController/FileIntegrityControllerTests/FileGroupTests.cs
@@ -1,10 +1,40 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using System.Collections.Generic;
+
+using FileIntegrityController;
+
 namespace FileIntegrityControllerTests
 {
     [TestClass]
     public class FileGroupTests
     {
+        [TestMethod]
+        public void Equals_EqualObjects_ReturnIsTrue()
+        {
+            // Arrange
+            FileGroup firstGroup = new FileGroup("D:\\", new Dictionary<string, string>() { { "1", "1" } });
+            FileGroup secondGroup = new FileGroup("D:\\", new Dictionary<string, string>() { { "1", "1" } });
 
+            // Act
+            bool actual = firstGroup.Equals(secondGroup);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void Equals_UnequalObjects_ReturnIsFalse()
+        {
+            // Arrange
+            FileGroup firstGroup = new FileGroup("D:\\", new Dictionary<string, string>() { { "1", "1" } });
+            FileGroup secondGroup = new FileGroup("D:\\", new Dictionary<string, string>() { { "1", "1" }, { "2", "2" } });
+
+            // Act
+            bool actual = firstGroup.Equals(secondGroup);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
     }
 }
