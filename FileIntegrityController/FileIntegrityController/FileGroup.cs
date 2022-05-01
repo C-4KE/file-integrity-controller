e47db7acb9a3a46b7e11c63b093db032fec5670a
C-4KE <glebkinkirill@gmail.com>
2022-05-01 13:48:10 +0300

    В FileGroup добавлен метод, разбивающий груупу на части.

--- FileIntegrityController/FileIntegrityController/FileGroup.cs
+++ FileIntegrityController/FileIntegrityController/FileGroup.cs
@@ -3,6 +3,9 @@ using System.Linq;
 
 namespace FileIntegrityController
 {
+    /**
+     * <summary>Класс, хранящий коллекцию пар (путь_к_файлу : хеш) для файлов, лежащих на одном диске.</summary>
+     */
     public class FileGroup
     {
         private string _diskName;
@@ -39,6 +42,38 @@ namespace FileIntegrityController
         public override bool Equals(object obj)
         {
             return (_diskName == ((FileGroup)obj)._diskName) && (_filesHashes.Count == ((FileGroup)obj)._filesHashes.Count) && (!_filesHashes.Except(((FileGroup)obj)._filesHashes).Any());
+        }
+
+        /**
+         * <summary>Метод, разделяющий группу на части.</summary>
+         * <param name="parts">Количество частей, на которые необходимо разбить группу.</param>
+         * <returns>Возвращает список из объектов FileGroup, являющиеся частями вызывающего метод объекта.</returns>
+         */
+        public List<FileGroup> Split(uint parts)
+        {
+            List<FileGroup> fileGroups = new List<FileGroup>();
+            if (parts > 1)
+            {
+                long elementsPerPart = _filesHashes.Count % parts == 0 ? _filesHashes.Count / parts : _filesHashes.Count / parts + 1;
+                FileGroup tempGroup = new FileGroup(_diskName, new Dictionary<string, string>());
+                long counter = 0;
+                foreach (KeyValuePair<string, string> fileHash in _filesHashes)
+                {
+                    tempGroup._filesHashes.Add(fileHash.Key, fileHash.Value);
+                    counter++;
+                    if (counter == elementsPerPart)
+                    {
+                        fileGroups.Add(tempGroup);
+                        tempGroup = new FileGroup(_diskName, new Dictionary<string, string>());
+                        counter = 0;
+                    }
+                }
+            }
+            else
+            {
+                fileGroups.Add(this);
+            }
+            return fileGroups;
         }
     }
 }
