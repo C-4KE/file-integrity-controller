7ae0dac28dfee9b43c1e576a25fa313b1f45e560
C-4KE <glebkinkirill@gmail.com>
2022-05-01 14:36:10 +0300

    Добавлены тесты для метода FileGroup.Split().

--- FileIntegrityController/FileIntegrityControllerTests/FileGroupTests.cs
+++ FileIntegrityController/FileIntegrityControllerTests/FileGroupTests.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using System.Collections.Generic;
+using System.Linq;
 
 using FileIntegrityController;
 
@@ -35,6 +36,70 @@ namespace FileIntegrityControllerTests
 
             // Assert
             Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void Split_OnePart_ReturnIsList()
+        {
+            // Arrange
+            FileGroup fileGroup = new FileGroup("D:\\", new Dictionary<string, string>() { { "1", "1" }, { "2", "2" } });
+            List<FileGroup> expected = new List<FileGroup>();
+            expected.Add(fileGroup);
+
+            // Act
+            List<FileGroup> actual = fileGroup.Split(1);
+
+            // Assert
+            Assert.IsTrue((actual.Count == expected.Count) && (!actual.Except(expected).Any()));
+        }
+
+        [TestMethod]
+        public void Split_MultiplePartsWithEqualNumberOfElements_ReturnIsList()
+        {
+            // Arrange
+            FileGroup fileGroup = new FileGroup("D:\\", new Dictionary<string, string>() { { "1", "1" }, { "2", "2" }, { "3", "3" }, { "4", "4" } });
+            List<FileGroup> expected = new List<FileGroup>();
+            expected.Add(new FileGroup("D:\\", new Dictionary<string, string>() { { "1", "1" }, { "2", "2" } }));
+            expected.Add(new FileGroup("D:\\", new Dictionary<string, string>() { { "3", "3" }, { "4", "4" } }));
+
+            // Act
+            List<FileGroup> actual = fileGroup.Split(2);
+
+            // Assert
+            Assert.IsTrue((actual.Count == expected.Count) && (!actual.Except(expected).Any()));
+        }
+
+        [TestMethod]
+        public void Split_MultiplePartsWithUnequalNumberOfElements_ReturnIsList()
+        {
+            // Arrange
+            FileGroup fileGroup = new FileGroup("D:\\", new Dictionary<string, string>() { { "1", "1" }, { "2", "2" }, { "3", "3" }, { "4", "4" }, { "5", "5" } });
+            List<FileGroup> expected = new List<FileGroup>();
+            expected.Add(new FileGroup("D:\\", new Dictionary<string, string>() { { "1", "1" }, { "2", "2" } }));
+            expected.Add(new FileGroup("D:\\", new Dictionary<string, string>() { { "3", "3" }, { "4", "4" } }));
+            expected.Add(new FileGroup("D:\\", new Dictionary<string, string>() { { "5", "5" } }));
+
+            // Act
+            List<FileGroup> actual = fileGroup.Split(3);
+
+            // Assert
+            Assert.IsTrue((actual.Count == expected.Count) && (!actual.Except(expected).Any()));
+        }
+
+        [TestMethod]
+        public void Split_MultiplePartsWithUnequalNumberOfElementsThatCantBeSplitByParts_ReturnIsListSplittedByPartsMinusOne()
+        {
+            // Arrange
+            FileGroup fileGroup = new FileGroup("D:\\", new Dictionary<string, string>() { { "1", "1" }, { "2", "2" }, { "3", "3" }, { "4", "4" } });
+            List<FileGroup> expected = new List<FileGroup>();
+            expected.Add(new FileGroup("D:\\", new Dictionary<string, string>() { { "1", "1" }, { "2", "2" } }));
+            expected.Add(new FileGroup("D:\\", new Dictionary<string, string>() { { "3", "3" }, { "4", "4" } }));
+
+            // Act
+            List<FileGroup> actual = fileGroup.Split(3);
+
+            // Assert
+            Assert.IsTrue((actual.Count == expected.Count) && (!actual.Except(expected).Any()));
         }
     }
 }
