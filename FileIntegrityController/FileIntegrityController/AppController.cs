8791b348c7551edb2f86d62657e7bcd604418c18
C-4KE <glebkinkirill@gmail.com>
2022-05-01 19:39:49 +0300

    Добавлен класс AppController, управляющий ходом выполнения программы.

---
+++ FileIntegrityController/FileIntegrityController/AppController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileIntegrityController
+{
+    /**
+     * <summary>Класс, управляющий ходом выполнения программы.</summary>
+     */
+    public class AppController
+    {
+    }
+}
