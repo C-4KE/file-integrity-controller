8c432a6b92f38ce73b041cda4ffa02b5aa82a636
C-4KE <glebkinkirill@gmail.com>
2022-05-01 17:30:19 +0300

    Добавлен класс, содержащий информацию для определения типа носителя, чтобы сократить количество запросов к ОС.

---
+++ FileIntegrityController/FileIntegrityController/StorageInfo.cs
@@ -0,0 +1,57 @@
+﻿using System.Management;
+
+namespace FileIntegrityController
+{
+    /**
+     * <summary>Класс, содержащий необходимые данные о носителях системы, чтобы было возможно было определить тип носителя.</summary>
+     */
+    public class StorageInfo
+    {
+        ManagementObjectSearcher _msftPartition;
+        ManagementObjectSearcher _msftDisk;
+        ManagementObjectSearcher _msftPhysicalDisk;
+
+        /**
+         * <value>Данные из класса MSFT_Partition</value>
+         */
+        public ManagementObjectSearcher MSFTPartition
+        {
+            get
+            {
+                return _msftPartition;
+            }
+        }
+
+        /**
+         * <value>Данные из класса MSFT_Disk</value>
+         */
+        public ManagementObjectSearcher MSFTDisk
+        {
+            get
+            {
+                return _msftDisk;
+            }
+        }
+
+        /**
+         * <value>Данные из класса MSFT_PhysicalDisk</value>
+         */
+        public ManagementObjectSearcher MSFTPhysicalDisk
+        {
+            get
+            {
+                return _msftPhysicalDisk;
+            }
+        }
+
+        /**
+         * <summary>Конструктор.</summary>
+         */
+        public StorageInfo()
+        {
+            _msftPartition = new ManagementObjectSearcher("root\\Microsoft\\Windows\\Storage", "SELECT * FROM MSFT_Partition");
+            _msftDisk = new ManagementObjectSearcher("root\\Microsoft\\Windows\\Storage", "SELECT * FROM MSFT_Disk");
+            _msftPhysicalDisk = new ManagementObjectSearcher("root\\Microsoft\\Windows\\Storage", "SELECT * FROM MSFT_PhysicalDisk");
+        }
+    }
+}
