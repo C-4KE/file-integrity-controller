65f01678bf52a3df950f192762ecfbaaf4b6379d
C-4KE <glebkinkirill@gmail.com>
2022-05-01 16:36:07 +0300

    Добавлен класс для тестов методов Distributor.

---
+++ FileIntegrityController/FileIntegrityControllerTests/DistributorTests.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using FileIntegrityController;
+
+namespace FileIntegrityControllerTests
+{
+    [TestClass]
+    class DistributorTests
+    {
+    }
+}
