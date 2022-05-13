d408881afdbcbb49fe4e2e3514ea79fbf242e096
C-4KE <glebkinkirill@gmail.com>
2022-05-13 22:38:37 +0300

    Добавлен изменённый класс IntegrityVerifier. Теперь при проверке целостности файла он его не сам открывает, а получает уже открытый файловый поток.

---
+++ FileIntegrityController/FileIntegrityController/IntegrityVerifier.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace FileIntegrityController
+{
+    /**
+     * <summary>Класс, отвечающий за проверку целостности файлов.</summary>
+     */
+    public class IntegrityVerifier
+    {
+        /**
+         * <summary>Метод, который проверяет целостность одного файла.</summary>
+         * <param name="fileStream">Открытый поток файла.</param>
+         * <param name="hash">Хэш файла для проверки целостности.</param>
+         * <returns>Возвращает true, если файл не изменён (снова подсчитанный хэш равен данному в Json), иначе - возвращает false.</returns>
+         */
+        public static bool VerifyFile(FileStream fileStream, string hash)
+        {
+            bool result = false;
+            string actualHash;
+            try
+            {
+                using (MD5 md5 = MD5.Create())
+                {
+                    byte[] byteHash = md5.ComputeHash(fileStream);
+                    actualHash = BitConverter.ToString(byteHash);
+                }
+                result = (actualHash == hash);
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine("Error while trying to compute hash: " + exc.Message);
+            }
+            return result;
+        }
+    }
+}
