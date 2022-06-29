818bf18800994020fd32ef9e0a7b767698af7d74
C-4KE <glebkinkirill@gmail.com>
2022-06-29 14:10:52 +0300

    Добавлен и настроен логгер

--- FileIntegrityController/FileIntegrityController/Program.cs
+++ FileIntegrityController/FileIntegrityController/Program.cs
@@ -2,9 +2,13 @@
 {
     class Program
     {
+        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
+            logger.Debug("Program has started");
             AppController.ManageApp();
+            NLog.LogManager.Shutdown();
         }
     }
 }
